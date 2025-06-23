148755b50361eeaeb0df8aba9ae43b9361268fd5
Dag <dagmara.szadkowska@wp.pl>
2025-06-23 17:51:53 +0200

    change project files

--- backend/database.py
+++ backend/database.py
@@ -1,26 +1,25 @@
 import urllib.parse
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 
-server = 'sanspacesrv.database.windows.net'
-database = 'SanSpaceDB'
-username = 'san'
-password = 'qwerty123!'
+server = os.getenv("DB_SERVER")
+database = os.getenv("DB_NAME")
+username = os.getenv("DB_USER")
+password = os.getenv("DB_PASSWORD")
 
 params = urllib.parse.quote_plus(
     f"DRIVER=ODBC Driver 18 for SQL Server;"
     f"SERVER={server};DATABASE={database};"
-    f"UID={username};PWD={password}"
+    f"UID={username};PWD={password};"
+    f"Encrypt=yes;TrustServerCertificate=no;"
 )
 
-
 engine = create_engine(f"mssql+pyodbc:///?odbc_connect={params}")
-
-
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-
 Base = declarative_base()
 
 def get_db():

ccecafa1bec2fd0f71bc2eb73698473e84ed96e7
Dag <dagmara.szadkowska@wp.pl>
2025-06-23 17:51:02 +0200

    update endpoints

--- backend/routers/rezerwacje.py
+++ backend/routers/rezerwacje.py
@@ -1,15 +1,16 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from database import get_db
-from models import Rezerwacja
-from schemas import RezerwacjaCreate, RezerwacjaResponse
+from models import Reservation
+from schemas import ReservationCreate, ReservationResponse
 
 router = APIRouter()
 
-@router.post("/rezerwacje", response_model=RezerwacjaResponse)
-def create_rezerwacja(rezerwacja: RezerwacjaCreate, db: Session = Depends(get_db)):
-    db_rez = Rezerwacja(**rezerwacja.dict())
-    db.add(db_rez)
+# !!! DO SPRAWDZENIA BO NIE DZIAŁA PRZEZ TRIGGERY W BAZIE
+@router.post("/reservations", response_model=ReservationResponse)
+def create_reservation(reservation: ReservationCreate, db: Session = Depends(get_db)):
+    db_reservation = Reservation(**reservation.dict())
+    db.add(db_reservation)
     db.commit()
-    db.refresh(db_rez)
-    return db_rez
+    db.refresh(db_reservation)
+    return db_reservation

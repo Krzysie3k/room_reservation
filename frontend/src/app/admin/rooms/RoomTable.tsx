48dd721e26ccf254adeae2d3a8d8368984f01957
blueteddy87 <blueteddy7@gmail.com>
2025-06-26 18:19:40 +0200

    RoomsFromBackend

--- frontend/src/app/admin/rooms/RoomTable.tsx
+++ frontend/src/app/admin/rooms/RoomTable.tsx
@@ -1,8 +1,8 @@
 type Room = {
   id: number;
   name: string;
-  capacity: number;
-  type: string;
+  seat_count: number;
+  room_type: { name: string };
   building: string;
   floor: string;
   description: string;
@@ -10,7 +10,7 @@ type Room = {
 
 export default function RoomTable({ rooms }: { rooms: Room[] }) {
   return (
-    <table className="min-w-full bg-white  rounded-lg shadow">
+    <table className="min-w-full bg-white rounded-lg shadow">
       <thead>
         <tr className="bg-gray-100 text-left">
           <th className="p-3">Nr</th>
@@ -26,9 +26,12 @@ export default function RoomTable({ rooms }: { rooms: Room[] }) {
           <tr key={room.id} className="border border-gray-200">
             <td className="p-3">{room.name}</td>
             <td className="p-3">
-              {room.type.charAt(0).toUpperCase() + room.type.slice(1)}
+              {room.room_type?.name
+                ? room.room_type.name.charAt(0).toUpperCase() +
+                  room.room_type.name.slice(1)
+                : "Brak typu"}
             </td>
-            <td className="p-3">{room.capacity}</td>
+            <td className="p-3">{room.seat_count}</td>
             <td className="p-3">{room.building}</td>
             <td className="p-3">{room.floor}</td>
             <td className="p-3">{room.description}</td>
--- frontend/src/app/admin/rooms/page.tsx
+++ frontend/src/app/admin/rooms/page.tsx
@@ -1,11 +1,32 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import RoomTable from "./RoomTable";
-import { mockRooms } from "@/mock/mockRooms";
+
+type Room = {
+  id: number;
+  name: string;
+  seat_count: number;
+  room_type: { name: string };
+  building: string;
+  floor: string;
+  description: string;
+};
 
 export default function RoomsPage() {
+  const [rooms, setRooms] = useState<Room[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/rooms")
+      .then((res) => res.json())
+      .then((data) => setRooms(data))
+      .catch((err) => console.error("Błąd przy pobieraniu sal:", err));
+  }, []);
+
   return (
     <div>
       <h1 className="text-xl font-semibold mb-4">Sale</h1>
-      <RoomTable rooms={mockRooms} />
+      <RoomTable rooms={rooms} />
     </div>
   );
 }
